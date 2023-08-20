a52280f792505a5cc7e2e2c1cf88ed1f78d205b6
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-20 15:15:12 +0300

    feat: update `GraphQLModule` service
    Add authorization support for `grapql-ws`

--- src/gql-config.service.ts
+++ src/gql-config.service.ts
@@ -5,12 +5,18 @@ import { Injectable } from '@nestjs/common';
 import { GqlOptionsFactory } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled';
+import { AuthService } from './auth/auth.service';
 
 @Injectable()
 export class GqlConfigService implements GqlOptionsFactory {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    private authService: AuthService,
+  ) {}
   createGqlOptions(): ApolloDriverConfig {
     const graphqlConfig = this.configService.get<GraphqlConfig>('graphql');
+    const getUserFromToken = async (token: string) =>
+      await this.authService.getUserFromToken(token);
     return {
       autoSchemaFile: graphqlConfig.schemaDestination || './src/schema.graphql',
       sortSchema: graphqlConfig.sortSchema,
@@ -19,7 +25,26 @@ export class GqlConfigService implements GqlOptionsFactory {
       },
       introspection: graphqlConfig.playgroundEnabled,
       subscriptions: {
-        'graphql-ws': true,
+        'graphql-ws': {
+          onConnect: async (context) => {
+            const { connectionParams, extra } = context;
+            if (!connectionParams?.authToken) {
+              console.error('No auth token passed');
+              return false;
+            }
+
+            const user = await getUserFromToken(
+              connectionParams.authToken as string,
+            );
+            if (!user) {
+              console.error('Auth token is invalid');
+              return false;
+            }
+
+            extra['user'] = user;
+            return true;
+          },
+        },
         'subscriptions-transport-ws': false,
       },
       includeStacktraceInErrorResponses: graphqlConfig.debug,
